c3d5b0db7580f3bf9880c7db7d880f053acb4b96
sci17 <recarzekaycee@gmail.com>
2024-12-01 22:15:40 +0700

    modified the chart.html, urls.py, and views.py

--- fireholder/projectsite/projectsite/urls.py
+++ fireholder/projectsite/projectsite/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView
+from fire.views import HomePageView, ChartView,PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country,multipleBarbySeverity
 from fire import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('dashborad_chart', ChartView.as_view(),name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
 
 ]
